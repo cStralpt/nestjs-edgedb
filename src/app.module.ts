b88e95229ee87a12a9c28b2c212abc8de03bccc6
Satria Alipatullah <satrialipatullah@gmail.com>
2023-12-25 13:01:55 +0700

    add: auth feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { UserModule } from './user/user.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodosModule, UserModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
